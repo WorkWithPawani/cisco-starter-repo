264fee6ed7b993206c0da2e3a0d25db9d9d806a1
WorkWithPawani <work.with.pawani@gmail.com>
2022-08-22 19:08:05 +0530

    TASK-3 display `User’s Public IP`

--- pylon/sextant/src/DataPointCardCollection.js
+++ pylon/sextant/src/DataPointCardCollection.js
@@ -1,20 +1,54 @@
+import React, { useEffect, useState } from "react";
 import {dataPoints} from './constants/dataPoints';
 import DataPointCard from './DataPointCard';
 import {textNoDataAvailable} from './constants/constants';
 import './DataPointCardCollection.css';
 
 function DataPointCardCollection() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [ipv4v6, setIpv4v6] = useState([]);
+    const [ipv4, setIpv4] = useState([]);
+    const ipDetails = [{"id": "ipv4", "name": "IPv4", "data": ipv4}, {"id": "ipv4v6", "name": "IPv4(v6)", "data": ipv4v6}]
+  
+    useEffect(() => {
+      fetch("https://api64.ipify.org?format=json")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setIpv4v6(result.ip);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
+        fetch("https://api.ipify.org?format=json")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setIpv4(result.ip);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  
     return (
         <div className="data-point-container">
         {/* {dataPoints.forEach((datapoint)=>{ return <DataPointCard name={datapoint.name} data={datapoint.data} />})} */}
-        {(dataPoints && dataPoints.length) ? dataPoints.map(dataPoint => {
+        {(ipDetails && ipDetails.length) ? ipDetails.map(ip => {
         return (
-          <div key={dataPoint.id}>
-            <DataPointCard name={dataPoint.name} data={dataPoint.data} id={dataPoint.id}/>
+          <div key={ip.id}>
+            <DataPointCard name={ip.name} data={ip.data} id={ip.id}/>
             <div className='data-point-card'>
-            <h2>name: {dataPoint.name}</h2>
+            <h2>IP Type: {ip.name}</h2>
             <hr />
-            <h2>data: {dataPoint.data}</h2>
+            <h2>IP Details: {ip.data}</h2>
             </div>
           </div>
         );
